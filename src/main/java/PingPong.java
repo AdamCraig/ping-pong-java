8750e0d131620cc37699a171e237aee47d1a8bb7
Adam Craig and Ashley Maceli <ashley@>
2016-04-18 15:38:01 -0700

    Add Spark and Velocity functionality so user can input number and get ping pong game results

--- src/main/java/PingPong.java
+++ src/main/java/PingPong.java
@@ -1,9 +1,36 @@
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class PingPong {
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    String layout = "templates/layout.vtl";
 
-  public ArrayList<Object> pingPong(Integer number) {
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/pingpong.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/output", (request,response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/output.vtl");
+
+      String number = request.queryParams("number");
+      Integer integerNumber = Integer.parseInt(number);
+      ArrayList<Object> pingPong = pingPong(integerNumber);
+
+      model.put("integerNumber", integerNumber);
+      model.put("pingPong", pingPong);
+      model.put("number", request.queryParams("number"));
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+  }
+
+  public static ArrayList<Object> pingPong(Integer number) {
     ArrayList<Object> countingUp = new ArrayList<Object>();
     for (Integer index = 1 ; index <= number ; index++) {
       if (index % 15 == 0) {
@@ -15,7 +42,8 @@ public class PingPong {
       } else {
         countingUp.add(index);
       }
-    } return countingUp;
+    }
+    return countingUp;
   }
 
 }
